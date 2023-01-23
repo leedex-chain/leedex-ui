0d1b57bd9c21c4e0345432ca966a945b3f97d6fb
Maxim [maxirmx] Samsonov <m.samsonov@computer.org>
2023-01-24 00:31:11 +0300

    Market implementation (4)

--- app/assets/asset-symbols/symbols.js
+++ app/assets/asset-symbols/symbols.js
@@ -2,8 +2,6 @@
 require("file-loader?name=asset-symbols/[name].png!./unknown.png");
 
 // Core asset
-require("file-loader?name=asset-symbols/[name].png!./gph.png");
-
 require("file-loader?name=asset-symbols/[name].png!./ld.png");
 
 // gpAssets
@@ -13,36 +11,15 @@ require("file-loader?name=asset-symbols/[name].png!./rub.png");
 require("file-loader?name=asset-symbols/[name].png!./cny.png");
 require("file-loader?name=asset-symbols/[name].png!./gold.png");
 require("file-loader?name=asset-symbols/[name].png!./silver.png");
-//require("file-loader?name=asset-symbols/[name].png!./btc.png");
 
 // RuDEX assets
 require("file-loader?name=asset-symbols/[name].png!./donate.png");
 require("file-loader?name=asset-symbols/[name].png!./dexbot.png");
 
 require("file-loader?name=asset-symbols/[name].png!./leedex.btc.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.ltc.png");
 require("file-loader?name=asset-symbols/[name].png!./leedex.usdt.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.eos.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.eth.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.golos.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.steem.png");
-
-require("file-loader?name=asset-symbols/[name].png!./rudex.pzm.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.nbs.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.xmr.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.bts.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.trx.png");
-
-require("file-loader?name=asset-symbols/[name].png!./rudex.dec.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.sps.png");
-
-require("file-loader?name=asset-symbols/[name].png!./rudex.bnb.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.busd.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.btcb.png");
-
-//Delisted
-require("file-loader?name=asset-symbols/[name].png!./rudex.ppy.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.smoke.png");
-require("file-loader?name=asset-symbols/[name].png!./rudex.wls.png");
+require("file-loader?name=asset-symbols/[name].png!./leedex.eth.png");
+require("file-loader?name=asset-symbols/[name].png!./leedex.trx.png");
+require("file-loader?name=asset-symbols/[name].png!./leedex.busd.png");
 
 // 3rd party assets
